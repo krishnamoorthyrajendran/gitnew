74e71ffad7d93819793a3e33bd85164323af1e72
krishnamoorthy <krishnamoorthy980@gmail.com>
2020-12-20 19:42:05 +0530

    this is commit msg

--- src/test/java/testing/appiumTest.java
+++ src/test/java/testing/appiumTest.java
@@ -7,11 +7,13 @@ public class appiumTest {
 	@Test
 	public void start() {
 		System.out.println("start1");
+		System.out.println("this is git new master1212 ");
 	}
     
 	@Test
 	public void start1() {
 		System.out.println("start1");
+		System.out.println("this is git new anther master ");
 		
 	}
 }
