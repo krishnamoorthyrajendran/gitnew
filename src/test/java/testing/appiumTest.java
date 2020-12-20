dfe619a7a118f867ecdc9a2336e84077994ddf53
krishnamoorthy <krishnamoorthy980@gmail.com>
2020-12-20 19:19:01 +0530

    intial commit

---
+++ src/test/java/testing/appiumTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+	
+	@Test
+	public void start() {
+		System.out.println("start1");
+	}
+    
+	@Test
+	public void start1() {
+		System.out.println("start1");
+		
+	}
+}
---
+++ src/test/java/testing/restapi.java
@@ -0,0 +1,18 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class restapi {
+
+	@Test
+	public void api() {
+		System.out.println("api");
+	}
+    
+	@Test
+	public void api1() {
+		System.out.println("api1");
+		
+	}
+
+}
