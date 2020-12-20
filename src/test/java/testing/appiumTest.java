25bf6838d9b6a3c50335dac8e0a426ef5c6f5927
krishnamoorthy <krishnamoorthy980@gmail.com>
2020-12-20 20:28:52 +0530

    this is develop

--- src/test/java/testing/appiumTest.java
+++ src/test/java/testing/appiumTest.java
@@ -9,6 +9,7 @@ public class appiumTest {
 		System.out.println("start1");
 		System.out.println("this is git new master1212 ");
 		System.out.println("this is git new gitx12 ");
+		System.out.println("developement ");
 	}
     
 	@Test
@@ -16,5 +17,13 @@ public class appiumTest {
 		System.out.println("start1");
 		System.out.println("this is git new anther master ");
 		System.out.println("this is git new gitx1545 ");
+		System.out.println("developement 123 ");
+	}
+	@Test
+	public void start146554() {
+		System.out.println("start1");
+		System.out.println("this is git new anther master ");
+		System.out.println("this is git new gitx1545 ");
+		System.out.println("developement 123 ");
 	}
 }
